2694fb985e6c1c69b5fcc7a5be7d537adf17543d
subhash pal <palsubhash046@gmail.com>
2026-01-14 10:25:40 +0530

    fix: clean reinstall and tailwind resolution

--- src/component/Nav.jsx
+++ src/component/Nav.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from "react";
 
 const Nav = () => {
-  return (
-    <div className='h-15 w-full bg-orange-400'>
-      hii ima nav
-    </div>
-  )
-}
+       return (
+              <div className="w-full">
+                     <div className="md:h-15 bg-gradient-to-r from-green-500 via-green-500 to-orange-500 flex  text-white font-semibold items-center flex-col md:flex-row">
+                            <img src="/grozo.png" alt="logo" className="h-20 w-30 md:h-4" />
+                            <form className="">
+                                   <input type="text" placeholder="Search"
+                                          className="p-2 border"
+                                   />
+                            </form>
+                     </div>
+              </div>
+       );
+};
 
-export default Nav
+export default Nav;
+
