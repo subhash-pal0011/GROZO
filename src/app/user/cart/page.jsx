4b36419fa06b7a239cf5c779a92de4d80af6ed16
subhash pal <palsubhash046@gmail.com>
2026-01-21 13:09:10 +0530

    Add dynamic cart discount and total calculation

--- src/app/user/cart/page.jsx
+++ src/app/user/cart/page.jsx
@@ -1,116 +1,5 @@
-// "use client";
-
-// import React from "react";
-// import { IoMdArrowBack } from "react-icons/io";
-// import { useRouter } from "next/navigation";
-
-// import { motion } from "framer-motion";
-// import { useSelector } from "react-redux";
-// import Image from "next/image";
-
-// const Page = () => {
-//        const router = useRouter();
-//        const { cartData } = useSelector((state) => state.card);
-//        console.log(`cartData : ${cartData}`)
-
-//        return (
-//               <div className="w-full min-h-screen flex items-center justify-center relative bg-gray-50 text-gray-700">
-
-//                      <div
-//                             onClick={() => router.push("/")}
-//                             className="flex items-center gap-1 absolute top-4 left-4 cursor-pointer"
-//                      >
-//                             <IoMdArrowBack size={25} />
-//                             <p className="font-semibold">Back</p>
-//                      </div>
-
-
-//                      {!cartData ? (
-//                             <motion.div
-//                                    initial={{
-//                                           y: 50,
-//                                           opacity: 0,
-//                                    }}
-//                                    animate={{
-//                                           y: 0,
-//                                           opacity: 1,
-//                                    }}
-//                                    transition={{
-//                                           duration: 0.6,
-//                                           ease: "easeOut",
-//                                    }}
-//                                    className="bg-linear-to-r from-green-50 via-green-50 to-orange-50 w-full justify-center flex flex-col items-center p-5 shadow-2xl shadow-orange-150">
-//                                    <Image src="/basket.gif" alt="basket" height={200} width={200} />
-//                                    <h3 className="text-xl font-semibold text-center">Your basket is empty!</h3>
-//                                    <button onClick={() => router.push("/")} className="border px-5 p-2 mt-3 text-sm rounded-xs bg-linear-to-r from-green-200 via-green-200 to-orange-200 text-gray-600 shadow shadow-orange-200 cursor-pointer">Shop now</button>
-//                             </motion.div>
-//                      )
-//                             :
-//                             (
-//                                    <div className="flex flex-col  md:flex-row gap-6 w-full p-5 mt-5">
-
-//                                           {/* LEFT */}
-//                                           <div className="flex flex-col gap-4 md:w-[70%] w-full  max-h-[80vh]
-//                                            overflow-y-auto bg-linear-to-r from-green-50 via-green-50 to-orange-50 p-2">
-//                                                  {cartData.map((item) => (
-//                                                         <div
-//                                                                key={item._id}
-//                                                                className="flex md:justify-between items-center bg-white p-4 rounded shadow justify-center"
-//                                                         >
-//                                                                <div className="flex md:flex-row flex-col gap-2 items-center">
-//                                                                       <Image
-//                                                                              src={item.image}
-//                                                                              alt={item.name}
-//                                                                              width={60}
-//                                                                              height={60}
-//                                                                       />
-//                                                                       <div className="space-y-0.5">
-//                                                                              <p className="font-semibold max-w-50 capitalize w-full text-gray-800">
-//                                                                                     {item.name}
-//                                                                              </p>
-
-//                                                                              <p className="text-xs text-gray-500">
-//                                                                                     {item.unit}
-//                                                                              </p>
-
-//                                                                              <p className="text-green-600 font-bold">
-//                                                                                     ₹{item.price}
-//                                                                              </p>
-//                                                                       </div>
-//                                                                       <button className="px-4 py-1 bg-green-200 rounded lg:hidden">
-//                                                                              Adde
-//                                                                       </button>
-
-//                                                                </div>
-
-//                                                                <button className="px-4 py-1 bg-green-200 rounded hidden lg:block">
-//                                                                       Add
-//                                                                </button>
-//                                                         </div>
-
-//                                                  ))}
-
-//                                           </div>
-
-//                                           {/* RIGHT */}
-//                                           <div className="md:w-[30%] w-full bg-white p-4 rounded shadow">
-//                                                  Summary Box
-//                                           </div>
-//                                    </div>
-//                             )
-//                      }
-
-
-//               </div>
-//        );
-// };
-
-// export default Page;
-
-
-
 "use client";
-import React from "react";
+import React, { useEffect } from "react";
 import { IoMdArrowBack } from "react-icons/io";
 import { useRouter } from "next/navigation";
 import { motion } from "framer-motion";
@@ -118,12 +7,19 @@ import { useDispatch, useSelector } from "react-redux";
 import Image from "next/image";
 import { IoIosAdd } from "react-icons/io";
 import { IoRemoveOutline } from "react-icons/io5";
-import { addCartData, removeCartData } from "@/redux/cartSlice";
+import { addCartData, calculateTotal, removeCartData } from "@/redux/cartSlice";
 
 const Page = () => {
        const router = useRouter();
-       const { cartData } = useSelector((state) => state.card);
+       const { cartData, totalPrice, deliveryCharge, platformCharge, grandTotal, discount } = useSelector((state) => state.card);
+       console.log(`discount : ${discount}`)
+
        const dispatch = useDispatch()
+
+       useEffect(() => {
+              dispatch(calculateTotal());
+       }, [cartData, dispatch]); //JESE-2 CARTDATA MEA ELEMENT AYEGA VESE-2 calculateTota CAll hoga
+
 
        const isEmpty = !cartData || cartData.length === 0;
 
@@ -131,13 +27,23 @@ const Page = () => {
               <div className="w-full min-h-screen relative bg-gray-50 text-gray-700">
 
                      {/* BACK BUTTON */}
-                     <div
+                     <motion.div
+                            initial={{
+                                   x: 100, opacity: 0
+                            }}
+                            animate={{
+                                   x: 0, opacity: 1
+                            }}
+                            transition={{
+                                   duration: 0.6, ease: "easeOut"
+                            }}
+
                             onClick={() => router.push("/")}
                             className="flex items-center gap-1 absolute top-4 left-4 cursor-pointer z-10"
                      >
                             <IoMdArrowBack size={22} />
                             <p className="font-semibold">Back</p>
-                     </div>
+                     </motion.div>
 
                      {/* EMPTY CART */}
                      {isEmpty ? (
@@ -155,7 +61,7 @@ const Page = () => {
                                    <button
                                           onClick={() => router.push("/")}
                                           className="mt-4 px-6 py-2 text-sm rounded
-                                          bg-linear-to-r from-green-200 via-green-200 to-orange-200 shadow shadow-orange-200"
+                                          bg-linear-to-r from-green-200 via-green-200 to-orange-200 shadow shadow-orange-200 cursor-pointer hover:shadow-2xl transition-all duration-300"
                                    >
                                           Shop now
                                    </button>
@@ -168,10 +74,19 @@ const Page = () => {
                                     max-h-[80vh] overflow-y-auto bg-linear-to-r from-gray-100 via-green-50 to-orange-50 p-3 rounded"
                                    >
                                           {cartData.map((item) => (
-                                                 <div
+                                                 <motion.div
+                                                        initial={{
+                                                               y: 60, opacity: 1
+                                                        }}
+                                                        animate={{
+                                                               y: 0, opacity: 1
+                                                        }}
+                                                        transition={{
+                                                               duration: 0.5, ease: "easeOut"
+                                                        }}
                                                         key={item._id}
                                                         className="flex md:justify-between justify-center
-                                                       items-center bg-white p-4 rounded shadow"
+                                                       items-center bg-white p-4 rounded shadow-xl"
                                                  >
                                                         <div className="flex md:flex-row flex-col gap-3 items-center w-full">
                                                                <Image
@@ -200,30 +115,91 @@ const Page = () => {
 
                                                                {/* MOBILE BUTTON */}
                                                                <div className="px-4 py-1 bg-green-200 rounded lg:hidden flex items-center gap-2">
-                                                                      <IoIosAdd className="cursor-pointer" size={25}/>
+                                                                      <IoIosAdd onClick={() => dispatch(addCartData(item))} size={25} className="cursor-pointer" />
                                                                       <span className="font-semibold">{item.qty}</span>
-                                                                      <IoRemoveOutline size={25}className="cursor-pointer"/>
+                                                                      <IoRemoveOutline onClick={() => dispatch(removeCartData(item._id))} size={25} className="cursor-pointer" />
                                                                </div>
                                                         </div>
 
                                                         {/* DESKTOP BUTTON */}
                                                         <div className="hidden lg:flex items-center gap-3 px-4 py-2 bg-linear-to-r from-green-100 via-green-100 to-orange-100 rounded">
-                                                               <IoIosAdd onClick={()=>dispatch(addCartData(item))} className="cursor-pointer" />
-                                                               <span className="font-semibold">{item.qty}</span>
-                                                               <IoRemoveOutline onClick={()=>dispatch(removeCartData(item._id))} className="cursor-pointer" />
-                                                        </div>
+                                                               <IoRemoveOutline onClick={() => dispatch(removeCartData(item._id))} className="cursor-pointer" />
 
-                                                 </div>
+                                                               <span className="font-semibold">{item.qty}</span>
+
+                                                               <IoIosAdd onClick={() => dispatch(addCartData(item))} className="cursor-pointer" />
+                                                        </div>
+                                                 </motion.div>
                                           ))}
                                    </div>
 
                                    {/* RIGHT : SUMMARY */}
-                                   <div className="md:w-[30%] w-full bg-linear-to-r from-orange-50 via-green-50 to-green-50 p-4 rounded shadow h-fit">
-                                          <h3 className="font-semibold text-lg mb-3">Summary</h3>
-                                          <p className="text-sm text-gray-600">
-                                                 Total items: {cartData.length}
-                                          </p>
-                                   </div>
+                                   <motion.div
+                                          initial={{
+                                                 y: -60, opacity: 1
+                                          }}
+                                          animate={{
+                                                 y: 0, opacity: 1
+                                          }}
+                                          transition={{
+                                                 duration: 0.5, ease: "easeOut"
+                                          }}
+
+                                          className="md:w-[30%] w-full bg-linear-to-r from-orange-50 via-green-50 to-green-50 p-2 rounded shadow-md h-fit space-y-2">
+                                          <h3 className="font-semibold text-xl text-center">Summary</h3>
+                                          <hr className="mt-2" />
+
+                                          <div className="w-full space-x-5 p-1 px-5 justify-between flex ">
+                                                 <span className="text-gray-600">Total items :</span>
+                                                 <span className="text-gray-800 font-semibold">{cartData?.length}</span>
+                                          </div>
+
+
+                                          <div className="w-full space-x-6 p-1  px-5 justify-between flex">
+                                                 <span className="text-gray-600">Total price :</span>
+                                                 <span className="text-gray-800 font-semibold">{totalPrice}</span>
+                                          </div>
+
+                                          <div className="w-full space-x-3 p-1  px-5 justify-between flex">
+                                                 <span className="text-gray-600">Platform Fee :</span>
+                                                 <span className="text-gray-800 font-semibold">{platformCharge}</span>
+                                          </div>
+
+                                          <div className="w-full space-x-0.5 p-1  px-5 justify-between flex">
+                                                 <span className="text-gray-600">Dilevery Charge :</span>
+                                                 <span className="text-gray-800 font-semibold">{deliveryCharge}</span>
+                                          </div>
+
+                                          <hr className="mt-4" />
+
+                                          <div className="w-full space-x-3 p-3 px-5 justify-between flex">
+                                                 <span className="text-green-600 text-xl">Total Amount :</span>
+                                                 <span className="text-gray-900 font-semibold">{grandTotal}</span>
+                                          </div>
+
+                                          {discount > 0 && (
+                                                 <motion.div
+                                                        initial={{
+                                                               opacity: 0, y: 10
+                                                        }}
+                                                        animate={{
+                                                               opacity: 1, y: 0
+                                                        }}
+                                                        transition={{
+                                                               duration: 0.4
+                                                        }}
+                                                        className="shadow-xl shadow-orange-200 p-2 rounded bg-green-50 text-center"
+                                                 >
+                                                        🎉<span className="text-green-600 text-sm font-semibold">
+                                                               Congratulations!
+                                                        </span>{" "}
+                                                        You saved{" "}
+                                                        <span className="text-green-600 text-xl font-semibold">
+                                                               ₹{discount}
+                                                        </span>
+                                                 </motion.div>
+                                          )}
+                                   </motion.div>
                             </div>
                      )}
               </div>
--- src/redux/cartSlice.js
+++ src/redux/cartSlice.js
@@ -1,52 +1,12 @@
-// import { createSlice } from "@reduxjs/toolkit";
-
-// const initialState = {
-//        cartData: [],
-// };
-
-// const cardSlice = createSlice({
-//        name: "card",
-//        initialState,
-//        reducers: {
-//               addCardData: (state, action) => {
-//                      const item = state.cartData.find(
-//                             (item) => item._id === action.payload._id
-//                      );
-
-//                      if (item) {
-//                             item.qty += 1;
-//                      } else {
-//                             state.cartData.push({ ...action.payload, qty: 1});
-//                      }
-//               },
-
-//               subtCardData: (state, action) => {
-//                      const item = state.cartData.find(
-//                             (item) => item._id === action.payload
-//                      );
-
-//                      if (!item) return;
-
-//                      if (item.qty > 1) {
-//                             item.qty -= 1;
-//                      } else {
-//                             state.cartData = state.cartData.filter(
-//                                    (p) => p._id !== action.payload
-//                             );
-//                      }
-//               },
-//        },
-// });
-
-// export const { addCardData, subtCardData } = cardSlice.actions;
-// export default cardSlice.reducer;
-
-
-
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
        cartData: [],
+       totalPrice: 0,
+       deliveryCharge: 40,
+       platformCharge: 5,
+       discount: 0,
+       grandTotal: 0,
 };
 
 const cartSlice = createSlice({
@@ -78,8 +38,30 @@ const cartSlice = createSlice({
                             );
                      }
               },
+
+              calculateTotal: (state) => {
+                     //                              reduce mea initial value batani padti hii isliye ,0
+                     state.totalPrice = state.cartData.reduce((sum, item) => sum + item.price * item.qty, 0 );
+
+                     state.deliveryCharge = state.totalPrice > 100 ? 5 : 40;
+
+                     let calculatedDiscount = 0;
+                     if (state.totalPrice >= 500) {
+                            calculatedDiscount = state.totalPrice * 0.10;
+                     } else if (state.totalPrice >= 300) {
+                            calculatedDiscount = state.totalPrice * 0.08; 
+                     } else if (state.totalPrice >= 150) {
+                            calculatedDiscount = state.totalPrice * 0.05; 
+                     }
+                     state.discount = Math.min(Math.floor(calculatedDiscount), 100);
+                     // 4️⃣ final amount
+                     state.grandTotal = state.totalPrice + state.deliveryCharge + state.platformCharge - state.discount;
+              }
+
        },
 });
 
-export const { addCartData, removeCartData } = cartSlice.actions;
+export const { addCartData, removeCartData, calculateTotal } = cartSlice.actions;
 export default cartSlice.reducer;
+
+
