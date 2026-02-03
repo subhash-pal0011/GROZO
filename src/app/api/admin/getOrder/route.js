c80e863e6628aa122a1cf6345516b08bdae504dd
subhash pal <palsubhash046@gmail.com>
2026-02-03 10:22:27 +0530

    remove console.log

--- src/app/api/admin/getOrder/route.js
+++ src/app/api/admin/getOrder/route.js
@@ -6,7 +6,17 @@ export async function GET() {
        try {
               await dbConnect();
 
-              const orders = await Order.find().populate("user").sort({createdAt:-1});
+              const orders = await Order.find()
+                     .populate("user")
+                     .populate({ // ITNA LMBA POPULATE KESE KR RHE HII PATA HII DEKHO ORDER SCHEMA KE UNDER orderAssignd HII ISKE UNDER deliveryBoyId HII , deliveryBoyId SE DELIVERY BOY KA SARA DATA NIKAL LENGE , 2 OPTION IRDE SCHEMA MEA HI DELIVERY BOY KI ID RKH DE DIRECT JESE USER KO POPULATE KRA RHE HII ESE ISE BHI KR LENGE
+                            path: "orderAssignd",
+                            populate: {
+                                   path: "deliveryBoyId",
+                                   select: "name mobile profilePic"
+                            }
+                     })
+                     .sort({ createdAt: -1 });
+
 
               if (orders.length === 0) {
                      return NextResponse.json(
