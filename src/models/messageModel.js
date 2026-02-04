71330c5141ff3bf3e7fc6d8b54085dc36d24fd10
subhash pal <palsubhash046@gmail.com>
2026-02-04 13:58:07 +0530

    Add chat and message mongoose models

---
+++ src/models/messageModel.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const messageSchema = new mongoose.Schema(
+       {
+              chatId: {
+                     type: mongoose.Schema.Types.ObjectId,
+                     ref: "ChatModel",
+                     required: true,
+              },
+
+              senderId: {
+                     type: mongoose.Schema.Types.ObjectId,
+                     ref: "User",
+                     required: true,
+              },
+
+              message: {
+                     type: String,
+                     required: true,
+                     trim: true,
+              },
+       },
+       { timestamps: true }
+);
+const MessageModel = mongoose.models.MessageModel || mongoose.model("MessageModel", messageSchema);
+export default MessageModel;
